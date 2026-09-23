99ed2a6adbbe17147da65ddbd52e67572333185c
natalia-escobar1 <naaat007@gmail.com>
2026-09-23 16:42:34 -0400

    remove classroom from login

---
+++ app/classroom/amuni/lesson-5/page.js
@@ -0,0 +1,6 @@
+import amuniLesson5 from "@/data/amuni-lesson5";
+import ClassroomView from "@/components/ClassroomView";
+
+export default function AmuniLesson3Page() {
+  return <ClassroomView topic={amuniLesson5} />;
+}
