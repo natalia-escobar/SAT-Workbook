67b0e2fd0047ce4e32d67bdd96e7a1c69f0a8b6d
natalia-escobar1 <naaat007@gmail.com>
2026-09-23 14:43:37 -0400

    adjusting and updating amuni's lesson 4 & 5

---
+++ data/amuni-lesson5.js
@@ -0,0 +1,44 @@
+import { asClassroom } from "@/lib/questions";
+
+const amuniLesson5 = {
+    name: "DDCUS-A Lesson 5",
+    problems: asClassroom(
+        [
+            "Par-XInt-2-E-1",
+            "Par-XInt-2-GP-1",
+            "Par-XInt-2-Q-1",
+            "Par-XInt-2-Q-2",
+            "Par-XInt-3-E-1",
+            "Par-XInt-3-GP-1",
+            "Par-XInt-3-Q-1",
+            "Par-XInt-4-E-1",
+            "Par-XInt-4-GP-1",
+            "Par-XInt-4-Q-1",
+            "Par-XInt-4-Q-2",
+            "Par-XInt-5-E-1",
+            "Par-XInt-5-GP-1",
+            "Par-XInt-5-Q-1",
+            "Par-XInt-5-Q-2",
+            "Par-XInt-6-E-1",
+            "Par-XInt-6-GP-1",
+            "Par-XInt-6-Q-1",
+            "Par-XInt-6-Q-2",
+            "Par-XInt-7-E-1",
+            "Par-XInt-7-GP-1",
+            "Par-XInt-7-Q-1",
+            "Par-XInt-7-Q-2",
+            "Par-XInt-8-E-1",
+            "Par-XInt-8-GP-1",
+            "Par-XInt-8-Q-1",
+            "Par-XInt-9-E-1",
+            "Par-XInt-9-AQ-2",
+            "Par-XInt-9-GP-1",
+            "Par-XInt-9-AQ-1",
+            "Par-XInt-9-Q-2",
+            "Par-XInt-9-Q-1",
+        ]
+    )
+
+};
+
+export default amuniLesson5;
